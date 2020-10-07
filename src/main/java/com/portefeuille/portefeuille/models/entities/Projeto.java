7f2d40912618c1eaffcef7e960c00d7d10ecc2fa
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-07 17:43:42 -0300

    mapeamento das entidades Projeto e Coordenador

---
+++ src/main/java/com/portefeuille/portefeuille/models/entities/Projeto.java
@@ -0,0 +1,48 @@
+package com.portefeuille.portefeuille.models.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "projeto")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Projeto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_projeto")
+  private int id;
+
+  @Column(name = "aluno")
+  private Long usuario;
+
+  @Column(name = "coordenador")
+  private Long coordenador;
+
+  @Column(name = "nome")
+  private String nome;
+
+  @Column(name = "tipo")
+  private String tipo;
+
+  @Column(name = "data")
+  private Date data;
+
+  @Column(name = "status")
+  private String status;
+
+  @Column(name = "descricao")
+  private String descricao;
+}
