4bccd3739f9022b35c31cc07a8a30ae0e27e39ac
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-16 18:36:09 -0300

    Correção de anotação
    inserida anotação Entity que estava faltando

--- src/main/java/com/portefeuille/portefeuille/models/entities/Coordenador.java
+++ src/main/java/com/portefeuille/portefeuille/models/entities/Coordenador.java
@@ -1,6 +1,7 @@
 package com.portefeuille.portefeuille.models.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +10,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Table(name = "coordenador")
 @Data
 @Builder
