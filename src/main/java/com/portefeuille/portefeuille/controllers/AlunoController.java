c69ba4f941a2dd5a280584744b95480d65dec730
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-18 11:32:48 -0300

    Update AlunoController.java
    endpoint para obter alunos por nome ou sobrenome

--- src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
+++ src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
@@ -2,38 +2,51 @@ package com.portefeuille.portefeuille.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.portefeuille.portefeuille.models.entities.Aluno;
 import com.portefeuille.portefeuille.services.AlunoService;
 
+import org.aspectj.apache.bcel.classfile.Module.Require;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/alunos")
+@RequestMapping("api/v1")
 public class AlunoController {
 
   @Autowired
   AlunoService service;
 
-  @GetMapping("/")
-  public ResponseEntity getAllAlunos() {
+  @GetMapping("/alunos")
+  public ResponseEntity getAllAlunos(@RequestParam(value = "nome", required = false) String nome,
+      @RequestParam(value = "sobrenome", required = false) String sobrenome) {
     List<Aluno> alunos;
+
+    System.out.println(nome + " " + sobrenome);
 
     try {
       alunos = service.obterAlunos();
+      if (nome != null) {
+        alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(nome)).collect(Collectors.toList());
+      }
+      if (sobrenome != null) {
+        alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(sobrenome)).collect(Collectors.toList());
+      }
       return new ResponseEntity<>(alunos, HttpStatus.OK);
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
 
-  @GetMapping("/{matricula}")
+  @GetMapping("/alunos/{matricula}")
   public ResponseEntity getAluno(@PathVariable Long matricula) {
     Optional<Aluno> aluno;
     try {
@@ -46,6 +59,7 @@ public class AlunoController {
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+
   }
 
 }
