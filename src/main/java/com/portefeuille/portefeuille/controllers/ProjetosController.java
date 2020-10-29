c72378c1e5d3cd394077e4500fbc5df07a8119bd
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-29 16:45:32 -0300

    Update ProjetosController.java
    atualizando enpoint para requisções de projetos por aluno

--- src/main/java/com/portefeuille/portefeuille/controllers/ProjetosController.java
+++ src/main/java/com/portefeuille/portefeuille/controllers/ProjetosController.java
@@ -8,10 +8,10 @@ import com.portefeuille.portefeuille.services.ProjetoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,7 +33,7 @@ public class ProjetosController {
     }
   }
 
-  @GetMapping("/{alunoMatricula}")
+  @GetMapping("/aluno/{alunoMatricula}")
   public ResponseEntity getProjetosByAluno(@PathVariable Long alunoMatricula) {
     List<Projeto> projetos;
 
