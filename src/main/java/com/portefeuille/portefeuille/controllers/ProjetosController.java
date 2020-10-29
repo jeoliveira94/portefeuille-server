ed820eec0a41faeb040b6896007ed67042ea222b
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-10-29 11:50:16 -0300

    Create ProjetosController.java
    criação do controlador de projetos com duas funções mapeadas inicialmente:
        - getProjetos -> retorna todos os projetos pelo endpoint raiz
        - getPorjetosByAluno -> retorna todos os projetos do aluno no endpoint raiz sendo passado a matricula do aluno como parametro de busca

---
+++ src/main/java/com/portefeuille/portefeuille/controllers/ProjetosController.java
@@ -0,0 +1,48 @@
+package com.portefeuille.portefeuille.controllers;
+
+import java.util.List;
+
+import com.portefeuille.portefeuille.models.entities.Projeto;
+import com.portefeuille.portefeuille.services.ProjetoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/projetos")
+public class ProjetosController {
+
+  @Autowired
+  ProjetoService service;
+
+  @GetMapping
+  public ResponseEntity getProjetos() {
+    List<Projeto> projetos;
+
+    try {
+      projetos = service.obterProjetos();
+      return new ResponseEntity<>(projetos, HttpStatus.OK);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @GetMapping("/{alunoMatricula}")
+  public ResponseEntity getProjetosByAluno(@PathVariable Long alunoMatricula) {
+    List<Projeto> projetos;
+
+    try {
+      projetos = service.obterProjetosDoAluno(alunoMatricula);
+      return new ResponseEntity<>(projetos, HttpStatus.OK);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+}
