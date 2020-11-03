83fff59d31d3971729cc09d976febe05c23da892
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-11-03 11:46:27 -0300

    Update AlunoController.java
    mapeamento de enpoint do aluno usado para autenticar

--- src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
+++ src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
@@ -3,6 +3,7 @@ package com.portefeuille.portefeuille.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import com.portefeuille.portefeuille.models.dto.AlunoDTO;
 import com.portefeuille.portefeuille.models.entities.Aluno;
 import com.portefeuille.portefeuille.services.AlunoService;
 
@@ -11,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +24,7 @@ public class AlunoController {
   @Autowired
   AlunoService service;
 
-  @GetMapping("/")
+  @GetMapping
   public ResponseEntity getAllAlunos() {
     List<Aluno> alunos;
 
@@ -46,6 +49,17 @@ public class AlunoController {
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @PostMapping("/autenticar")
+  public ResponseEntity authAluno(@RequestBody AlunoDTO dto) {
+    Aluno aluno = Aluno.builder().nome(dto.getNome()).senha(dto.getSenha()).area(dto.getArea())
+        .dataNascimento(dto.getDataNascimento()).matricula(dto.getMatricula()).build();
+
+    if (service.autenticarAluno(aluno.getMatricula(), aluno.getSenha())) {
+      return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(false, HttpStatus.OK);
   }
 
 }
