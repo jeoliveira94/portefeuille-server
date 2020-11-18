6a12cb87c0e94dfe1384eee8e5efff243e6a85da
Weverton Trindade <weverton2trindade@gmail.com>
2020-11-18 14:55:55 -0300

    Atualizado AlunoController.java
    Adicionado conversões de tipo para os dados da classe AlunoDTO.

--- src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
+++ src/main/java/com/portefeuille/portefeuille/controllers/AlunoController.java
@@ -1,5 +1,7 @@
 package com.portefeuille.portefeuille.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
@@ -27,58 +29,63 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("api/v1/alunos")
 public class AlunoController {
 
-  @Autowired
-  AlunoService service;
+	@Autowired
+	AlunoService service;
 
-  @GetMapping
-  public ResponseEntity getAllAlunos(@RequestParam(value = "nome", required = false) String nome,
-      @RequestParam(value = "sobrenome", required = false) String sobrenome) {
-    List<Aluno> alunos;
+	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 
-    System.out.println(nome + " " + sobrenome);
+	@GetMapping
+	public ResponseEntity getAllAlunos(@RequestParam(value = "nome", required = false) String nome,
+			@RequestParam(value = "sobrenome", required = false) String sobrenome) {
+		List<Aluno> alunos;
 
-    try {
-      alunos = service.obterAlunos();
-      if (nome != null) {
-        alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(nome)).collect(Collectors.toList());
-      }
-      if (sobrenome != null) {
-        alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(sobrenome)).collect(Collectors.toList());
-      }
-      return new ResponseEntity<>(alunos, HttpStatus.OK);
-    } catch (Exception e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
-  }
+		System.out.println(nome + " " + sobrenome);
 
-  @GetMapping("/{matricula}")
-  public ResponseEntity getAluno(@PathVariable Long matricula) {
-    Optional<Aluno> aluno;
-    try {
-      aluno = service.obterAluno(matricula);
-      if (aluno.isPresent()) {
-        return new ResponseEntity<>(aluno, HttpStatus.OK);
-      } else {
-        throw new Exception();
-      }
-    } catch (Exception e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
+		try {
+			alunos = service.obterAlunos();
+			if (nome != null) {
+				alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(nome)).collect(Collectors.toList());
+			}
+			if (sobrenome != null) {
+				alunos = alunos.stream().filter(aluno -> aluno.getNome().contains(sobrenome))
+						.collect(Collectors.toList());
+			}
+			return new ResponseEntity<>(alunos, HttpStatus.OK);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 
-  }
+	@GetMapping("/{matricula}")
+	public ResponseEntity getAluno(@PathVariable Long matricula) {
+		Optional<Aluno> aluno;
+		try {
+			aluno = service.obterAluno(matricula);
+			if (aluno.isPresent()) {
+				return new ResponseEntity<>(aluno, HttpStatus.OK);
+			} else {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 
-  @PostMapping
-  public ResponseEntity postAluno(@RequestBody AlunoDTO dto) {
+	}
 
-    try {
-      Aluno novoAluno = Aluno.builder().matricula(dto.getMatricula()).nome(dto.getNome())
-          .dataNascimento(dto.getDataNascimento()).area(dto.getArea()).build();
+	@PostMapping
+	public ResponseEntity postAluno(@RequestBody AlunoDTO dto) {
 
-      Aluno alunoSalvo = service.salvarAluno(novoAluno);
-      return new ResponseEntity<>(alunoSalvo, HttpStatus.OK);
-    } catch (Exception e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
-  }
+		try {
+			Long matricula = Long.valueOf(dto.getMatricula());
+			Date data = formato.parse(dto.getDataNascimento());
+			Aluno novoAluno = Aluno.builder().matricula(matricula).nome(dto.getNome()).dataNascimento(data)
+					.area(dto.getArea()).build();
+
+			Aluno alunoSalvo = service.salvarAluno(novoAluno);
+			return new ResponseEntity<>(alunoSalvo, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 
 }
