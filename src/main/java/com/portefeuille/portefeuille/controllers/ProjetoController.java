6e63f72453cde99fb613c3e183f188a05231d408
Weverton Trindade <weverton2trindade@gmail.com>
2020-11-20 12:09:11 -0300

    Criado requisição post em ProjetoController
    Método salvarProjeto implementado.

--- src/main/java/com/portefeuille/portefeuille/controllers/ProjetoController.java
+++ src/main/java/com/portefeuille/portefeuille/controllers/ProjetoController.java
@@ -1,7 +1,10 @@
 package com.portefeuille.portefeuille.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+import com.portefeuille.portefeuille.models.dto.ProjetoDTO;
 import com.portefeuille.portefeuille.models.entities.Projeto;
 import com.portefeuille.portefeuille.services.ProjetoService;
 
@@ -10,16 +13,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/projetos")
-public class ProjetosController {
+@RequestMapping("/api/v1/projetos/")
+public class ProjetoController {
 
   @Autowired
   ProjetoService service;
+
+  SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+
+  @PostMapping
+  public ResponseEntity postProjeto(@RequestBody ProjetoDTO dto) throws Exception {
+    Long alunoMatricula = Long.valueOf(dto.getAlunoMatricula());
+    Long coordenadorMatricula = Long.valueOf(dto.getCoordenadorMatricula());
+    Date data = formato.parse(dto.getData());
+
+    try {
+      Projeto projeto = Projeto.builder().alunoMatricula(alunoMatricula).coordenadorMatricula(coordenadorMatricula)
+          .nome(dto.getNome()).tipo(dto.getTipo()).data(data).status(dto.getStatus()).descricao(dto.getDescricao())
+          .build();
+
+      Projeto projetoSalvo = service.salvarProjeto(projeto);
+      return new ResponseEntity<>(projetoSalvo, HttpStatus.CREATED);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
 
   @GetMapping
   public ResponseEntity getProjetos() {
---
+++ src/test/java/com/portefeuille/controller/ProjetoControllerTest.java
@@ -0,0 +1,67 @@
+package com.portefeuille.controller;
+
+import java.text.SimpleDateFormat;
+
+import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portefeuille.portefeuille.PortefeuilleApplication;
+import com.portefeuille.portefeuille.models.dto.ProjetoDTO;
+import com.portefeuille.portefeuille.models.entities.Projeto;
+import com.portefeuille.portefeuille.services.ProjetoService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("teste")
+@SpringBootTest(classes = PortefeuilleApplication.class)
+@AutoConfigureMockMvc
+public class ProjetoControllerTest {
+	static final String api = "/api/v1/projetos/";
+
+	@Autowired
+	MockMvc mvc;
+
+	@Autowired
+	ProjetoService service;
+
+	SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+
+	@Test
+	void deveSalvarProjeto() throws Exception {
+		ProjetoDTO dto = ProjetoDTO.builder().alunoMatricula("123").coordenadorMatricula("23").nome("wev").tipo("xyz")
+				.data("04/02/2001").status("t").descricao("abcd").build();
+
+		System.out.println(dto);
+
+		Projeto projeto = Projeto.builder().alunoMatricula(Long.valueOf(dto.getAlunoMatricula()))
+				.coordenadorMatricula(Long.valueOf(dto.getCoordenadorMatricula())).nome(dto.getNome())
+				.tipo(dto.getTipo()).data(formato.parse(dto.getData())).status(dto.getStatus())
+				.descricao(dto.getDescricao()).build();
+
+		System.out.println(projeto);
+
+		Mockito.when(service.salvarProjeto(Mockito.any(Projeto.class))).thenReturn(projeto);
+		String json = new ObjectMapper().writeValueAsString(dto);
+
+		System.out.println(json);
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(api).accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON).content(json);
+
+		System.out.println(request);
+
+		mvc.perform(request).andExpect(MockMvcResultMatchers.status().isCreated());
+	}
+}
